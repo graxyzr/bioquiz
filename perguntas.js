506c7ef8c733293d14ecd6d614920afa00935919
Prietsch <106382323+Prietsch@users.noreply.github.com>
2024-07-26 13:42:35 -0300

    Create perguntas.js

---
+++ perguntas.js
@@ -0,0 +1,94 @@
+//criar uma array e passar o número, as questões, opções e respostas;
+const questions = [
+  {
+    numb: 1,
+    question:
+      '<img src="https://static.significados.com.br/foto/argentina.jpg" >',
+    answer: "Argentina",
+    options: ["Uruguai", "Chile", "Argentina", "Suriname"],
+  },
+  {
+    numb: 2,
+    question:
+      '<img src="https://upload.wikimedia.org/wikipedia/commons/6/60/Flag_of_Suriname.svg">',
+    answer: "Suriname",
+    options: ["Chile", "Paraguai", "Peru", "Suriname"],
+  },
+  {
+    numb: 3,
+    question:
+      '<img src="https://upload.wikimedia.org/wikipedia/commons/7/7b/Flag_of_Venezuela_%28state%29.svg">',
+    answer: "Venezuela",
+    options: ["Venezuela", "Peru", "Paraguai", "Uruguai"],
+  },
+  {
+    numb: 4,
+    question:
+      '<img src="https://www.gov.br/planalto/pt-br/conheca-a-presidencia/acervo/simbolos-nacionais/bandeira/bandeiragrande.jpg">',
+    answer: "Brasil",
+    options: ["Suriname", "Brasil", "Argentina", "Colômbia"],
+  },
+  {
+    numb: 5,
+    question:
+      '<img src="https://upload.wikimedia.org/wikipedia/commons/2/21/Flag_of_Colombia.svg">',
+    answer: "Colômbia",
+    options: ["Colômbia", "Guiana", "Equador", "Chile"],
+  },
+  {
+    numb: 6,
+    question:
+      '<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/Flag_of_Peru_%28state%29.svg/1280px-Flag_of_Peru_%28state%29.svg.png">',
+    answer: "Peru",
+    options: ["Suriname", "Paraguai", "Venezuela", "Peru"],
+  },
+  {
+    numb: 7,
+    question:
+      '<img src="https://s1.static.brasilescola.uol.com.br/be/conteudo/images/bandeira-guiana-55e5be841ef7e.jpg">',
+    answer: "Guiana",
+    options: ["Argentina", "Guiana", "Colômbia", "Uruguai"],
+  },
+  {
+    numb: 8,
+    question:
+      '<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/de/Flag_of_Bolivia_%28state%29.svg/1280px-Flag_of_Bolivia_%28state%29.svg.png">',
+    answer: "Bolívia",
+    options: ["Chile", "Guiana", "Bolívia", "Equador"],
+  },
+  {
+    numb: 9,
+    question:
+      '<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/29/Flag_of_French_Guiana.svg/255px-Flag_of_French_Guiana.svg.png">',
+    answer: "Guiana Francesa",
+    options: ["Guiana Francesa", "Chile", "Equador", "Brasil"],
+  },
+  {
+    numb: 10,
+    question:
+      '<img src="https://s5.static.brasilescola.uol.com.br/be/2022/11/bandeira-equador.jpg">',
+    answer: "Equador",
+    options: ["Colômbia", "Guiana", "Equador", "Guiana"],
+  },
+  {
+    numb: 11,
+    question:
+      '<img src="https://static.todamateria.com.br/upload/pa/ra/paraguai.jpg">',
+    answer: "Paraguai",
+    options: ["Chile", "Guiana Francesa", "Bolívia", "Paraguai"],
+  },
+  {
+    numb: 12,
+    question:
+      '<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/fe/Flag_of_Uruguay.svg/1024px-Flag_of_Uruguay.svg.png">',
+    answer: "Uruguai",
+    options: ["Colômbia", "Brasil", "Uruguai", "Chile"],
+  },
+  {
+    numb: 13,
+    question:
+      '<img src="https://static.todamateria.com.br/upload/ba/nd/bandeiradochile-cke.jpg">',
+    answer: "Chile",
+    options: ["Brasil", "Suriname", "Venezuela", "Chile"],
+  },
+];
